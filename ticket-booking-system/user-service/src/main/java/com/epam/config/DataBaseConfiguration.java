1704207729a963ae5c3bd992eed7ac772d5da5f3
Anton Tarianyk <anton_tarianyk@epam.com>
2017-07-17 12:26:37 +0300

    config

--- ticket-booking-system/user-service/src/main/java/com/epam/config/DataBaseConfiguration.java
+++ ticket-booking-system/user-service/src/main/java/com/epam/config/DataBaseConfiguration.java
@@ -62,7 +62,7 @@ public class DataBaseConfiguration {
 
     Properties additionalProperties() {
         Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", "create");
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
         properties.setProperty("hibernate.show_sql", "true");
         return properties;
