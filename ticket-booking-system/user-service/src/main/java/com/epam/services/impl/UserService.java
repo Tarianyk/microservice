86167fa1fdf0f3660f8dd876e6fa523a03e1cbb6
Anton Tarianyk <anton_tarianyk@epam.com>
2017-07-17 10:39:22 +0300

    added init service

--- ticket-booking-system/user-service/src/main/java/com/epam/services/impl/UserService.java
+++ ticket-booking-system/user-service/src/main/java/com/epam/services/impl/UserService.java
@@ -1,11 +1,22 @@
 package com.epam.services.impl;
 
 import com.epam.domain.User;
+import com.epam.dto.UserDto;
+import com.epam.repository.UserRepository;
 import com.epam.services.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserService implements IUserService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ConversionService conversionService;
 
     @Override
     public User getUserById(long userId) {
@@ -23,8 +34,8 @@ public class UserService implements IUserService {
     }
 
     @Override
-    public User createUser(User user) {
-        return null;
+    public User createUser(UserDto userDto) {
+        return userRepository.save(conversionService.convert(userDto, User.class));
     }
 
     @Override
